ad7e3a48ac75674f0c235ec43d9d72c7713ba74a
deivi18contreras <deivibogoya@gmail.com>
2025-11-13 20:14:35 -0500

    componente card

---
+++ src/components/cards.vue
@@ -0,0 +1,18 @@
+<template>
+   <div> 
+    <div title = "" color = "" > </div>
+   </div> 
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+
+const props = defineProps ({
+    title:{
+        type: String,
+        description: String,
+        
+    }
+})
+</script>
