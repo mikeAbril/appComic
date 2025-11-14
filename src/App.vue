53d69541da8943f81b0744ef92adb11b29ccb47e
deivi18contreras <deivibogoya@gmail.com>
2025-11-13 21:23:46 -0500

    Card prueba

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <template>
-<div class="principal">
-  <footer>
-    <div class="q-pa-md">
-        <div class="q-gutter-y-md" style="max-width: 600px">
-          <q-tabs
-            v-model="tab"
-            class="text-teal"
-          >
-            <q-tab name="Comidas" icon="alarm" label="Alarms" />
-            <q-tab name="Entrenamientos" icon="movie" label="Movies" />
-          </q-tabs>
-        <!-- contenido   -->
-          <q-tab-panels v-model="tab" animated>
-            <q-tab-panel name="mails">
-              <h4>Mails content</h4>
-            </q-tab-panel>
-
-            <q-tab-panel name="comidas">
-              <h4>Alarms content</h4>
-            </q-tab-panel>
-
-            <q-tab-panel name="entrenamientos">
-              <h4>Movies content</h4>
-            </q-tab-panel>
-          </q-tab-panels>
+    <div class="principal">
+        <div class="card">
+            <cards title="Desayuno" :image="desayuno" colorFondo=""></cards>
         </div>
+        <div class="card">
+            <cards title="Almuerzo" :image="almuerzo" colorFondo=""></cards>
+        </div>
+        <div class="card">
+            <cards title="Cena" :image="cena" colorFondo=""></cards>
+        </div>
+        <footer>
+            <div class="q-pa-md">
+                <div class="q-gutter-y-md" style="max-width: 600px">
+                    <q-tabs v-model="tab" class="text-teal">
+                        <q-tab name="Comidas" icon="alarm" label="Alarms" />
+                        <q-tab name="Entrenamientos" icon="movie" label="Movies" />
+                    </q-tabs>
+                    <!-- contenido   -->
+                    <q-tab-panels v-model="tab" animated>
+                        <q-tab-panel name="mails">
+                            <h4>Mails content</h4>
+                        </q-tab-panel>
+
+                        <q-tab-panel name="comidas">
+                            <h4>Alarms content</h4>
+                        </q-tab-panel>
+
+                        <q-tab-panel name="entrenamientos">
+                            <h4>Movies content</h4>
+                        </q-tab-panel>
+                    </q-tab-panels>
+                </div>
+            </div>
+        </footer>
     </div>
-  </footer>
-</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { QPagination } from 'quasar';
+import cards from './components/cards.vue';
 import cards from './components/cards.vue';
 import desayuno from './assets/desayuno.png'
 import almuerzo from './assets/almuerzo.png'
@@ -45,7 +51,7 @@ const tab = ref('comidas')
 
 
 <style>
-.card{
+.card {
     border: solid 2px red;
     border-radius: 10px;
     width: 400px;
