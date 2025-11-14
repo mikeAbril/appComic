6d92b32d2fe108a6e2aab3140672d7afa6761bc8
deivi18contreras <deivibogoya@gmail.com>
2025-11-13 21:04:01 -0500

    Card prueba

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
 <div class="principal">
+    <div class="card">
+        <cards title="Desayuno" :image="desayuno" colorFondo =""></cards>
+    </div>
+    <div class="card">
+        <cards title="Almuerzo" :image="almuerzo" colorFondo =""></cards>
+    </div>
+    <div class="card">
+        <cards title="Cena" :image="cena" colorFondo =""></cards>
+    </div>
+
   <footer>
      <div class="q-pa-lg flex flex-center">
     <QPagination
@@ -17,10 +27,19 @@
 <script setup>
 import { ref } from 'vue';
 import { QPagination } from 'quasar';
+import cards from './components/cards.vue';
+import desayuno from './assets/desayuno.png'
+import almuerzo from './assets/almuerzo.png'
+import cena from './assets/cena.png'
 
 const current = ref(5)
 </script>
 
 
 <style>
+.card{
+    border: solid 2px red;
+    border-radius: 10px;
+    width: 400px;
+}
 </style>
--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,23 +1,28 @@
 <template>
-   <div> 
-    <div title = "" colorFondo = "" > </div>
-   </div> 
+    <div :style="{ backgroundColor: props.colorFondo }">
+    <h2>{{ props.title }}</h2>
+    <img v-if="props.image" :src="props.image" alt="Imagen" />
+  </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
 
 
-const props = defineProps ({
-    title:{
-        type: String,
-        description: String,      
-    },
-    coloFondo:{
+const props = defineProps({
+    title: {
         type: String,
         required: true,
         default: ""
     },
-    
+    image: {
+        type: String,
+    },
+    colorFondo: {
+        type: String,
+        required: true,
+        default: ""
+    },
+
 })
 </script>
